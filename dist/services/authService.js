817d264660abf1865b5946b0474432751e38b599
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-04-18 01:48:24 +0300

    commit

---
+++ dist/services/authService.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authService = void 0;
+class AuthService {
+}
+exports.authService = new AuthService();
+//# sourceMappingURL=authService.js.map
