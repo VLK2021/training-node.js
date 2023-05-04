92e8a43a89fe7bb63d357381dbdfc55b8753f085
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-05-04 20:49:49 +0300

    lesson15_sockets

--- src/app.ts
+++ src/app.ts
@@ -66,19 +66,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(mainRouter);
 
 const {PORT} = process.env;
-// app.listen(PORT, async () => {
-//     console.log(`Server is started on PORT:${PORT}!`);
-//     try {
-//         const connection = await createConnection();
-//         if (connection) {
-//             console.log('Database connection!');
-//             // cronRun()
-//         }
-//     } catch (err) {
-//         if (err) console.log(err);
-//     }
-// });
-
 
 server.listen(PORT, async () => {
     console.log(`Server is started on PORT:${PORT}!`);
