7f360663439e3a36d698e7c4932b00e644d9b54d
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-05-07 08:15:39 +0300

    lesson17_start creating_swagger

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import swaggerUI from 'swagger-ui-express';
 
 import { authRouter } from './authRouter';
 import { postRouter } from './postRouter';
 import { userRouter } from './userRouter';
+// @ts-ignore
+import * as docs from '../docs/swagger.json';
 
 const router = Router();
 
+router.use('/docs', swaggerUI.serve, swaggerUI.setup(docs));
 router.use('/users', userRouter);
 router.use('/posts', postRouter);
 router.use('/auth', authRouter);
