84e4ff57b35080f7b8d083b802a6994949e61d53
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-05-08 15:38:06 +0300

    lesson17_swagger

--- src/cron/get-new-users.ts
+++ src/cron/get-new-users.ts
@@ -2,7 +2,9 @@ import cron from 'node-cron';
 import { userRepository } from '../repositories/user/userRepository';
 
 export const getNewUsers = async () => {
+
     cron.schedule('*/10 * * * * *', async () => {
+
         console.log('START WORK WITH GET NEW USERS');
         const newUsers = await userRepository.getNewUsers();
 
